bb90525254580fdde9eff2130df9def10334eb9a
Feralbot <staaasyyaaan@gmail.com>
2023-01-09 20:43:42 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/fintess/" : "/",
-};
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/fintess/" : "/",
 });
